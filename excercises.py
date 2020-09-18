f485f6715a0aecd4e524152e48f4b2f9e5d8732f
Bensas <juanbensa@gmail.com>
2020-09-18 20:57:49 -0300

    Fixed excercise 1, removed extra weight, added excercise pdf

--- excercises.py
+++ excercises.py
@@ -2,28 +2,27 @@ import numpy as np
 
 def excercise1():
   x = [[-1, 1], [1, -1], [-1, -1], [1, 1]]
-  x = np.array(x)
   y = [-1, -1, -1, 1]
+
+  x = np.array(x)
   y = np.array(y)
 
   print(x)
   print(y)
-  print("WTF")
-  perceptron = Perceptron(4)
+  perceptron = Perceptron(2)
   perceptron.train(x, y)
   print(perceptron.weights)
-
-
+  # print(np.dot(perceptron.weights, [-1, 1]))
 
 class Perceptron(object):
 
     def __init__(self, no_of_inputs, threshold=100, learning_rate=0.01):
         self.threshold = threshold
         self.learning_rate = learning_rate
-        self.weights = np.zeros(no_of_inputs + 1)
+        self.weights = np.zeros(no_of_inputs)
            
     def predict(self, inputs):
-        summation = np.dot(inputs, self.weights[1:]) + self.weights[0]
+        summation = np.dot(inputs, self.weights) + self.weights[0]
         if summation > 0:
           activation = 1
         else:
@@ -34,8 +33,7 @@ class Perceptron(object):
         for _ in range(self.threshold):
             for inputs, label in zip(training_inputs, labels):
                 prediction = self.predict(inputs)
-                self.weights[1:] += self.learning_rate * (label - prediction) * inputs
-                self.weights[0] += self.learning_rate * (label - prediction)
+                self.weights[0:] += self.learning_rate * (label - prediction) * inputs
 
 
 excercise1()
