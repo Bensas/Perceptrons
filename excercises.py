94c951e45856d45572d0b78db7dc87bcc8b4d8fd
Bensas <juanbensa@gmail.com>
2020-09-19 12:34:34 -0300

    Fixed result verification (we weren't including the bias 🙄

--- excercises.py
+++ excercises.py
@@ -7,38 +7,53 @@ def excercise1():
   x = np.array(x)
   y = np.array(y)
 
-  print(x)
-  print(y)
+  print("=== OR FUNCTION ===")
+  print("X=" + str(x))
+  print("Y=" + str(y))
+  print("Training...")
   perceptron = Perceptron(2)
   perceptron.train(x, y)
-  print(perceptron.weights)
-  print(np.dot(perceptron.weights, [-1, 1]) < 0)
-  print(np.dot(perceptron.weights, [1, -1]) < 0)
-  print(np.dot(perceptron.weights, [-1, -1]) < 0)
-  print(np.dot(perceptron.weights, [1, 1]) > 0)
+  print("Resulting weights: " + str(perceptron.weights))
+  print(perceptron.predict([-1, 1]) < 0)
+  print(perceptron.predict([1, -1]) < 0)
+  print(perceptron.predict([-1, -1]) < 0)
+  print(perceptron.predict([1, 1]) > 0)
+
+  # print("=== XOR FUNCTION ===")
+  # y = [1, 1, -1, -1]
+  # y = np.array(y)
+  # print("X=" + str(x))
+  # print("Y=" + str(y))
+  # print("Training...")
+  # perceptron = Perceptron(2)
+  # perceptron.train(x, y)
+  # print("Resulting weights: " + str(perceptron.weights))
+  # print(perceptron.predict([-1, 1]) > 1)
+  # print(perceptron.predict([1, -1]) > 1)
+  # print(perceptron.predict([-1, -1]) < 0)
+  # print(perceptron.predict([1, 1]) < 0)
+
 
 class Perceptron(object):
 
-    def __init__(self, no_of_inputs, threshold=100, learning_rate=0.01):
+    def __init__(self, no_of_inputs, threshold=50, learning_rate=0.01):
         self.threshold = threshold
         self.learning_rate = learning_rate
         self.weights = np.zeros(no_of_inputs)
-        self.bias = 0.01;
+        self.bias = 0.01
            
     def predict(self, inputs):
         summation = np.dot(inputs, self.weights) + self.bias
-        if summation > 0:
+        if summation >= 0:
           return 1
-        elif summation < 0:
-          return -1
         else:
-          return 0
+          return -1
 
     def train(self, training_inputs, labels):
         for _ in range(self.threshold):
             for inputs, label in zip(training_inputs, labels):
                 prediction = self.predict(inputs)
-                print("Prediction: " + str(prediction) + ", label: " + str(label))
+                # print("Prediction: " + str(prediction) + ", label: " + str(label))
                 self.weights += self.learning_rate * (label - prediction) * inputs
                 self.bias += self.learning_rate * (label - prediction)
                 # print(self.weights);
