48fc67c7703045ccefaaead95f8bdf42329c59cb
Bensas <juanbensa@gmail.com>
2020-09-20 00:19:43 -0300

    Added excercise 3 code with nonlinear perceptron, still doesn't work

--- excercises.py
+++ excercises.py
@@ -1,29 +1,11 @@
 import csv
 import numpy as np
 from simple_perceptron import SimplePerceptron
+from non_linear_perceptron import NonLinearPerceptron
 
 def excercise1():
   x = [[-1, 1], [1, -1], [-1, -1], [1, 1]]
   y = [-1, -1, -1, 1]
-
-  # y_values = []
-  # f = open('ej2_Y.txt', 'r')
-  # reader = csv.reader(f, delimiter=' ', lineterminator='\n')
-  # for row in reader:
-  #     for each in row:
-  #         if (each != ''):
-  #             y_values.append(each)
-  # f.close()
-  # x_values = []
-  # f = open('ej2_X.txt', 'r')
-  # reader = csv.reader(f, delimiter=' ', lineterminator='\n')
-  # for row in reader:
-  #     x = []
-  #     for each in row:
-  #         if(each != ''):
-  #             x.append(each)
-  #     x_values.append(x)
-  # f.close()
 
   x = np.array(x)
   y = np.array(y)
@@ -50,34 +32,38 @@ def excercise1():
 
 
 def excercise2():
-  x = [[-1, 1], [1, -1], [-1, -1], [1, 1]]
-  y = [-1, -1, -1, 1]
+  x = []
+  f = open('ej2_X.txt', 'r')
+  reader = csv.reader(f, delimiter=' ', lineterminator='\n')
+  for row in reader:
+      x_row = []
+      for each in row:
+          if(each != ''):
+              x_row.append(float(each))
+      x.append(x_row)
+  f.close()
+
+  y = []
+  f = open('ej2_Y.txt', 'r')
+  reader = csv.reader(f, delimiter=' ', lineterminator='\n')
+  for row in reader:
+      for each in row:
+          if (each != ''):
+              y.append(float(each))
+  f.close()
 
   x = np.array(x)
   y = np.array(y)
 
-  print("=== OR FUNCTION ===")
+  # print("=== OR FUNCTION ===")
   print("X=" + str(x))
   print("Y=" + str(y))
   print("Training...")
-  perceptron = SimplePerceptron(2)
+  perceptron = NonLinearPerceptron(3)
   perceptron.train(x, y)
+  print(perceptron.cost_function(x, y))
+  # print_perceptron_test(perceptron, x, y)
   print("Resulting weights: " + str(perceptron.weights))
-  print_perceptron_test(perceptron, x, y)
-
-  # print("=== XOR FUNCTION ===")
-  # y = [1, 1, -1, -1]
-  # y = np.array(y)
-  # print("X=" + str(x))
-  # print("Y=" + str(y))
-  # print("Training...")
-  # perceptron = Perceptron(2)
-  # perceptron.train(x, y)
-  # print("Resulting weights: " + str(perceptron.weights))
-  # print(perceptron.predict([-1, 1]) > 1)
-  # print(perceptron.predict([1, -1]) > 1)
-  # print(perceptron.predict([-1, -1]) < 0)
-  # print(perceptron.predict([1, 1]) < 0)
 
 def print_perceptron_test(perceptron, inputs, expected_outputs):
   print("Perceptron tests (all should be true):")
@@ -85,4 +71,6 @@ def print_perceptron_test(perceptron, inputs, expected_outputs):
     print(perceptron.predict(curr_input) == curr_expected_output)
 
 
-excercise1()
+excercise2()
+
+# 4.47, -4.08, 4.45 -> 87
